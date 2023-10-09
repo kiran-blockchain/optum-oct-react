92f8a664b221bc295164b3c78f3aaab67238c05d
BRUGUSOFT <kiran.paturi@brugu.io>
2023-10-09 13:03:39 +0530

    added Header

---
+++ ecommerce/src/App.jsx
@@ -0,0 +1,12 @@
+import { Header } from "./components/Header";
+import  {HeaderConfig}  from "./utils/config";
+
+const App = () => {
+  return (
+    <div>
+      <Header headerConfig ={HeaderConfig}/>
+    </div>)
+};
+
+export default App;
+
---
+++ ecommerce/src/utils/config.js
@@ -0,0 +1,28 @@
+export const HeaderConfig = {
+    title: "E-Commerce App",
+    navItems: [{
+        index:0,
+        name: "Login",
+        url: "/login"
+    },
+    {
+        index:1,
+        name: "Register",
+        url: "/register"
+    },
+    {
+        index:2,
+        name: "Products",
+        url: "/products"
+    },
+    {
+        index:3,
+        name: "Cart",
+        url: "/cart"
+    },
+    {
+        index:4,
+        name: "Logout",
+        url: "/logout"
+    },]
+};
